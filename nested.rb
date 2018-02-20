ddfcecc6ad030fe6047c5babd122cc7e62f0295c
Chase <chaseit12@gmail.com>
2018-02-20 02:01:47 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,21 +16,7 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+	programmer_hash = 'C'
 end
 
 def adding_matz
